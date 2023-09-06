441bcd7058cb446365c0848b1a63efeff9099382
Gayathrimanikanti <144203545+Gayathrimanikanti@users.noreply.github.com>
2023-09-06 11:44:32 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+#create the main application window
+root = tk.Tk()
+
+#set the window title
+root.title("My Tkinter App for python")
+
+#set the window size
+root.geometry("400x200")
+
+#start the Tkinter event loop
+root.mainloop()
